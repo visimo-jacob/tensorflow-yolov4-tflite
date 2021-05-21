e7230a31d1cc7ec3e413bcba0b11dcb6ae464eaf
Jacob Leisey-Bartsch <jacob@visimo.ai>
2021-05-21 10:39:14 -0400

    Ability to turn off gpu support (because it wasnt detecting after the first frame with gpy support on). Still working on that one

--- detect_video.py
+++ detect_video.py
@@ -1,6 +1,13 @@
 import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
+######################################################################
+# This will turn the GPU off
+######################################################################
+physical_devices = []
+tf.config.set_visible_devices([], 'GPU')
+######################################################################
+
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags, logging
